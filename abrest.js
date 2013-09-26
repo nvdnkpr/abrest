d0c4d93f65de344994108db35abfb2c734bc3769
James Costian <james@jamescostian.com>
2013-09-25 22:31:28 -0500

    Began writing a closure

--- abrest.js
+++ abrest.js
@@ -1,5 +1,16 @@
-abrest = function () {
-    var api = this
+(function (scope) {
+    var abrest = function () {
+        var api = this
 
-    return api
-}
+        return api
+    }
+
+    if (typeof define === 'function' && define.amd) {
+        define(function () {
+            return abrest
+        })
+    }
+    else {
+        scope.abrest = abrest
+    }
+}(this))
