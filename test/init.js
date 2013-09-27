a72823393b76fc6832260c2b77c6b46c9169254f
James Costian <james@jamescostian.com>
2013-09-27 17:53:12 -0500

    Added more tests

--- test/init.js
+++ test/init.js
@@ -2,9 +2,19 @@ mocha.ui('bdd')
 mocha.reporter('html')
 expect = chai.expect
 
-describe('abrest', function() {
-    it('should be attached to the global scope by default', function () {
-        expect(abrest).to.not.be.undefined
+describe('Abrest', function() {
+    describe('basic behavior tests', function () {
+        it('should be attached to the global scope by default', function () {
+            expect(Abrest).to.not.be.undefined
+        })
+        it('should use defaults when no info is given', function () {
+            var instance = new Abrest()
+            expect(instance).to.not.be.undefined
+            expect(instance.timeout).to.equal(1000)
+            expect(instance.baseURL).to.equal('')
+            expect(instance.defaultHeaders).to.be.an.object
+            expect(instance.defaultData).to.be.an.object
+        })
     })
 })
 
