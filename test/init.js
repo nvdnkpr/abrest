4139053a5f914b58488290e6edd33dd2347f11f9
James Costian <james@jamescostian.com>
2013-09-27 19:45:55 -0500

    Added tests for combineObjs

--- test/init.js
+++ test/init.js
@@ -3,6 +3,26 @@ mocha.reporter('html')
 expect = chai.expect
 
 describe('Abrest', function() {
+    describe('helpers', function () {
+        var instance = new Abrest() // Instance variable that's share amongst all of the helper tests
+
+        describe('combineObjs', function () {
+            var defaults = {'default thing': true, 'over-write me': false},
+                overrides = {'over-write me': true, 'foo': 'bar'},
+                combinations = instance.combineObjs(defaults, overrides)
+
+            it('should inherit from the defaults', function () {
+                expect(combinations['default thing']).to.equal(true)
+            })
+            it('should get stuff from the overrides', function () {
+                expect(combinations['foo']).to.equal('bar')
+            })
+            it('should allow the overrides to override the defaults', function () {
+                expect(combinations['over-write me']).to.equal(true)
+            })
+        })
+
+    })
     describe('basic behavior tests', function () {
         it('should be attached to the global scope by default', function () {
             expect(Abrest).to.not.be.undefined
